e111fb5e7dc914faf52a59a369e995fcf04734ee
diggibyte-kesavan <kesavan.k@diggibyte.com>
2024-03-12 11:22:08 +0530

    add the assertion in test cases

--- test/percentage_problem/percentagetest.py
+++ test/percentage_problem/percentagetest.py
@@ -6,13 +6,13 @@ class MyTestCase(unittest.TestCase):
     def test_case(self):
         actual_input = percent_calculation({'Kesavan': [67, 68, 69], 'Aravind': [70, 98, 63], 'Bala': [52, 56, 60]}, 'Kesavan')
         expected_output = 68
-        self.assertEqual(actual_input, format(expected_output, '.2f'))
-        # self.assertEqual(True, False)  # add assertion here
+        self.assertEqual(actual_input, format(expected_output, '.2f'))   # add the assertion here
+        # self.assertEqual(True, False)
 
     def test_case2(self):
         actual_input = percent_calculation({'Priya': [25, 26.5, 28], 'Kishore': [26, 28, 30]}, 'Priya')
         expected_output = 26.5
-        self.assertEqual(actual_input, format(expected_output, '.2f'))
+        self.assertEqual(actual_input, format(expected_output, '.2f'))   # add the assertion here
 
 
 if __name__ == '__main__':
