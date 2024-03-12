c5c5bc14b4a778eb1f1ea7bf5e6e8d15df9e8688
diggibyte-kesavan <kesavan.k@diggibyte.com>
2024-03-13 01:53:54 +0530

    added the second test case function

--- test/calendermodule_problem/calendermoduletest.py
+++ test/calendermodule_problem/calendermoduletest.py
@@ -8,7 +8,10 @@ class MyTestCase(unittest.TestCase):
         expected_output = "WEDNESDAY"
         self.assertEqual(actual_output, expected_output)  # add assertion here
 
-
+    def test_case2(self):
+        actual_output = find_day_of_date("08 06 2020")
+        expected_output = "THURSDAY"
+        self.assertEqual(actual_output, expected_output)
 
 
 if __name__ == '__main__':
