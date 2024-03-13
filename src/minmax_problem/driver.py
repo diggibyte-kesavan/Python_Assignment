931e2d0d5f5df15c7d375cd22637f554e06b6b54
diggibyte-kesavan <kesavan.k@diggibyte.com>
2024-03-13 10:35:38 +0530

    added the test case2 in min_max problem

--- src/minmax_problem/driver.py
+++ src/minmax_problem/driver.py
@@ -1,4 +1,4 @@
 from src.minmax_problem.util import min_max_axis_1
-matrix = [[4,2], [2,5], [3,7],[1,3],[4,0]]
+matrix = [[4, 3], [8, 7], [2, 7], [6, 2], [4, 1]]
 result = min_max_axis_1(matrix)
 print(result)
--- test/minmax_problem/minmaxtest.py
+++ test/minmax_problem/minmaxtest.py
@@ -1,12 +1,19 @@
 import unittest
 from src.minmax_problem.util import min_max_axis_1
 
+
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
+    def test_case(self):
         matrix = [[4, 2], [2, 5], [3, 7], [1, 3], [4, 0]]
         actual_output = min_max_axis_1(matrix)
         expected_output = 3
         self.assertEqual(actual_output, expected_output)  # add assertion here
+
+    def test_case1(self):
+        matrix = [[4, 3], [8, 7], [2, 7], [6, 2], [4, 1]]
+        actual_output = min_max_axis_1(matrix)
+        expected_output = 7
+        self.assertEqual(actual_output, expected_output)
 
 
 if __name__ == '__main__':
