569fe744d0b79bfb991f5d2c3299b73ce911c999
diggibyte-kesavan <kesavan.k@diggibyte.com>
2024-03-13 13:48:58 +0530

    added the test case2 function and checks the cases

--- src/wordorder_problem/driver.py
+++ src/wordorder_problem/driver.py
@@ -1,3 +1,4 @@
 from src.wordorder_problem.util import word_order
 print(word_order(4,['bcdef', 'abcdefg', 'bcde', 'bcdef']))
+print(word_order(5, ['apple', 'banana', 'apple', 'orange', 'banana']))
 
