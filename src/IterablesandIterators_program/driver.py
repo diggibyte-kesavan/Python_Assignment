4fee1b9955032a7c708d2ec60c95d2858133b253
diggibyte-kesavan <kesavan.k@diggibyte.com>
2024-03-13 17:09:32 +0530

    added the case2 file in IterablesandIteratorstest file

--- src/IterablesandIterators_program/driver.py
+++ src/IterablesandIterators_program/driver.py
@@ -1,2 +1,2 @@
 from src.IterablesandIterators_program.util import iterables_iterators
-print(iterables_iterators(4,"aacd",2))
+print(iterables_iterators(5, "aaeed", 2))
--- test/IterablesandIterators_problems/IterablesandIteratorstest.py
+++ test/IterablesandIterators_problems/IterablesandIteratorstest.py
@@ -3,10 +3,15 @@ from src.IterablesandIterators_program.util import iterables_iterators
 
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
+    def test_case(self):
         actual_output = iterables_iterators(4, "aacd", 2)
         expected_output = 0.8333333
-        self.assertEqual(actual_output, expected_output)  # add assertion here
+        self.assertEqual(actual_output, expected_output)
+
+    def test_case1(self):
+        actual_output = iterables_iterators(5, "aaeed", 2)
+        expected_output = 0.7
+        self.assertEqual(actual_output, expected_output)
 
 
 if __name__ == '__main__':
